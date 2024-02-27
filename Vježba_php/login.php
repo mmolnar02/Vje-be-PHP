68fc6ec9f4af11bcb33370f2771c035d340f31d3
Marko <mmarko1g@gmail.com>
2024-02-27 12:46:05 +0100

    Vježba PHP

---
+++ Vježba_php/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vježba PHP</title>
+</head>
+<body>
+    <h1>Login</h1>
+</body>
+</html>
+
+<?php
+    include_once "obrazac.php";
+?>
---
+++ Vježba_php/obrazac.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+<body>
+    <table>
+         <form action="">
+            <tr>
+                <td>Ime:</td>
+                <td><input type="text" name="Ime"></td>
+            </tr>
+            <tr>
+                <td>Prezime:</td>
+                <td><input type="text" name="Prezime"></td>
+            </tr>
+            <tr>
+                <td>E-mail:</td>
+                <td><input type="email" name="E-mail" id=""></td>
+            </tr>
+            <tr>
+                <td>Lozinka</td>
+                <td><input type="password" name="Lozinka" id=""></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button>Pošalji!</button>
+                </td>
+             </tr>
+         </form>
+    </table>
+
+</body>
+</html>
